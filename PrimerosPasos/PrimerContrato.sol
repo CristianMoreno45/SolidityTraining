84d905f21d69bed622d7f7a24d76f042202f0971
cristian_moreno <cristian.moreno@juju.com.co>
2022-01-25 09:30:25 -0500

    add comments in natspec

--- PrimerosPasos/PrimerContrato.sol
+++ PrimerosPasos/PrimerContrato.sol
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: MIT
 
-/**
+/// Comentario estándar natspec
+/// @title      Contrato ABC
+/// @author     Cristiaan Moreno
+/// @notice     El contrato airve para ABC
+/// @dev        Detalles adicionales
+/// @param      nombre_parametro que funcion tiene en el contrato
+/// @return     valor_retornado para que sirve el valor devuelto
 
-*/
+
 pragma solidity >=0.8.0;
 
 contract PrimerContrato {
